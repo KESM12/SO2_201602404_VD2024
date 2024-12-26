0da83fd107173240730cbb98d599daefcfcc28c0
KESM12 <3001920290101@ingenieria.usac.edu.gt>
2024-12-26 07:50:15 -0600

    [UPDATE]
    Se cambio el valor de prueba para tamalloc.

--- Pruebas/tarotamalloc_test.c
+++ Pruebas/tarotamalloc_test.c
@@ -13,7 +13,7 @@ int main() {
     printf("Implementación de Tamalloc para asignación de memoria. Presione ENTER para continuar...\n");
     getchar();
 
-    size_t total_size = 100 * 1024 * 1024; // 100 MB
+    size_t total_size = 21 * 1024 * 1024; // 20 MB
 
     // Use the tamalloc syscall
     char *buffer = (char *)syscall(SYS_TAMALLOC, total_size);
